b7ee5e66eb788cf1b4794ebb7cd5f39f8c0c6c5d
aidant <15520814+aidant@users.noreply.github.com>
2025-05-28 17:26:25 +1000

    feat(tsgo): refactor to remove todo!

--- src/tsgo.rs
+++ src/tsgo.rs
@@ -2,81 +2,94 @@ use std::fs;
 
 use zed_extension_api::{self as zed, Result, settings::LspSettings};
 
-struct TsGoExtension {
-    server_path: String,
-    package_name: String,
-    cached_binary_path: Option<String>,
-}
+struct CurrentPlatform(zed::Os, zed::Architecture);
 
-impl TsGoExtension {
-    fn server_exists(&self) -> bool {
-        fs::metadata(self.server_path.clone()).map_or(false, |stat| stat.is_file())
-    }
-    fn binary_path(&mut self, id: &zed::LanguageServerId) -> Result<String> {
-        let server_exists = self.server_exists();
-        if self.cached_binary_path.is_some() && server_exists {
-            return Ok(self.server_path.clone());
-        }
-
-        zed::set_language_server_installation_status(
-            id,
-            &zed::LanguageServerInstallationStatus::CheckingForUpdate,
-        );
-        let version = zed::npm_package_latest_version(&self.package_name)?;
-
-        if !server_exists
-            || zed::npm_package_installed_version(&self.package_name)?.as_ref() != Some(&version)
-        {
-            zed::set_language_server_installation_status(
-                id,
-                &zed::LanguageServerInstallationStatus::Downloading,
-            );
-            let result = zed::npm_install_package(&self.package_name, &version);
-            match result {
-                Ok(()) => {
-                    if !self.server_exists() {
-                        Err(format!(
-                            "installed package '{}' did not contain expected path '{}'",
-                            &self.package_name, &self.server_path
-                        ))?;
-                    }
-                }
-                Err(error) => {
-                    if !self.server_exists() {
-                        Err(error)?;
-                    }
-                }
-            }
-        }
-        Ok(self.server_path.clone())
-    }
-}
-
-impl zed::Extension for TsGoExtension {
-    fn new() -> Self {
-        let (platform, arch) = zed::current_platform();
-
-        let platform = match platform {
+impl CurrentPlatform {
+    pub fn get_package_name_and_server_path(&self) -> Result<(String, String)> {
+        let platform = match self.0 {
             zed::Os::Linux => "linux",
             zed::Os::Mac => "darwin",
             zed::Os::Windows => "win32",
         };
 
-        let arch = match arch {
+        let arch = match self.1 {
             zed::Architecture::Aarch64 => "arm64",
-            zed::Architecture::X86 => todo!(),
+            zed::Architecture::X86 => return Err("32-bit architecture is not supported".into()),
             zed::Architecture::X8664 => "x64",
         };
 
         let package_name = format!("@typescript/native-preview-{platform}-{arch}");
         let server_path = format!(
             "node_modules/{package_name}/lib/tsgo{}",
-            if platform == "win32" { ".exe" } else { "" }
+            if self.0 == zed::Os::Windows {
+                ".exe"
+            } else {
+                ""
+            }
         );
 
+        Ok((package_name, server_path))
+    }
+}
+
+struct TsGoExtension {
+    current_platform: CurrentPlatform,
+    cached_binary_path: Option<String>,
+}
+
+impl TsGoExtension {
+    fn server_exists(&self, server_path: &str) -> bool {
+        fs::metadata(server_path).map_or(false, |stat| stat.is_file())
+    }
+
+    fn binary_path(&mut self, id: &zed::LanguageServerId) -> Result<String> {
+        let (package_name, server_path) =
+            self.current_platform.get_package_name_and_server_path()?;
+        let server_exists = self.server_exists(&server_path);
+        if self.cached_binary_path.is_some() && server_exists {
+            return Ok(server_path.clone());
+        }
+
+        zed::set_language_server_installation_status(
+            id,
+            &zed::LanguageServerInstallationStatus::CheckingForUpdate,
+        );
+        let version = zed::npm_package_latest_version(&package_name)?;
+
+        if !server_exists
+            || zed::npm_package_installed_version(&package_name)?.as_ref() != Some(&version)
+        {
+            zed::set_language_server_installation_status(
+                id,
+                &zed::LanguageServerInstallationStatus::Downloading,
+            );
+            let result = zed::npm_install_package(&package_name, &version);
+            match result {
+                Ok(()) => {
+                    if !self.server_exists(&server_path) {
+                        Err(format!(
+                            "installed package '{}' did not contain expected path '{}'",
+                            &package_name, &server_path
+                        ))?;
+                    }
+                }
+                Err(error) => {
+                    if !self.server_exists(&server_path) {
+                        Err(error)?;
+                    }
+                }
+            }
+        }
+        Ok(server_path.clone())
+    }
+}
+
+impl zed::Extension for TsGoExtension {
+    fn new() -> Self {
+        let (os, arch) = zed::current_platform();
+
         Self {
-            server_path,
-            package_name,
+            current_platform: CurrentPlatform(os, arch),
             cached_binary_path: None,
         }
     }
